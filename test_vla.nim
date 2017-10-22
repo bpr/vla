73fa26065f7a57508d56c6dd77dd8ac96f58a4dc
Brent Pedersen <bpederse@gmail.com>
2017-10-21 19:54:01 -0600

    inlining array access gives huge improvement in speed.

--- test_vla.nim
+++ test_vla.nim
@@ -3,38 +3,46 @@ import vla, times
 let 
   s = "Warning: unknown magic 'DeepCopy' might crash the compiler [UnknownMagic]"
 
-var count = 0
+var 
+  vla_count = 0
+  seq_count = 0
 
-proc test_oa(n : int, a: openarray[char]) = 
+proc test_oa(count : var int, a: openarray[char]) =
   count += len(a)
 
-proc test_vla(n : int) = 
+proc test_vla(n : int) =
   var vla = newVLA(char, n)
   for i in countup(0,n-1):
     vla[i] = s[i]
-  test_oa(n, asOpenArray(vla))
+  test_oa(vla_count, asOpenArray(vla))
 
-proc test_seq(n : int) = 
-  var seqVal : seq[char] = @[]
+proc test_seq(n : int) =
+  var seqVal = new_seq[char](n)
   for i in countup(0,n-1):
-    seqVal.add(s[i])
-  test_oa(n, seqVal)
+    seqVal[i] = s[i]
+  test_oa(seq_count, seqVal)
 
 # Time the VLA
 var t0 = cpuTime()
 for n in 0..100000:
   for i in 0..len(s):
     test_vla(i)
-var t1 = cpuTime()
-echo("alloca time = ", (t1 - t0))
+
+var vla_time = cpuTime() - t0
+echo("alloca time = ", vla_time)
+echo("vla count = ", vla_count)
 
 # Time Nim's built in seq
+t0 = cpuTime()
 for n in 0..100000:
   for i in 0..len(s):
     test_seq(i)
-var t2 = cpuTime()
-echo("seq time = ", (t2 - t1))
-echo("count = ", count)
+
+var seq_time = cpuTime() - t0
+echo("seq time = ", seq_time)
+echo("seq count = ", seq_count)
+
+echo("speedup = ", float64(seq_time) / float64(vla_time))
 
 discard r"""
 proc test_ragged_vla(nrows:int, ncols:int) = 
